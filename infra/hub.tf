e3b470bde73f3c842a8abcd92bf08666e81fda42
saiyidi <issiway@gmail.com>
2023-09-20 00:03:16 +0200

    tags in hub

--- infra/hub.tf
+++ infra/hub.tf
@@ -3,5 +3,8 @@ module "west-eu-hub-01" {
   rg_name                = "drs-ghost-hub-rg01"
   location               = "westeurope"
   vnet_address_space     = ["192.168.1.0/24"]
-  default_subnet_address = ["192.168.1.0/26"] 
+  default_subnet_address = ["192.168.1.0/26"]
+  tags = {    
+    "Application" = "Ghost"
+  }
 }
